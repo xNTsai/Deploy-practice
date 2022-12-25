810c87dca27f6c1a87997a4b4472309a81deee93
xNTsai <johnnytsai920315@gmail.com>
2022-12-25 21:22:27 +0800

    api fix

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import ScoreCard from './models/ScoreCard.js'
 import routes from './src/routes';
 import db from './src/db';
 
 
   const app = express();
   const port = process.env.PORT || 4000;
-  if (process.env.NODE_ENV === "development") {
+  // if (process.env.NODE_ENV === "development") {
     app.use(cors());
-  }
+  // }
   app.use(express.json())
   app.listen(port, () =>                                   
     console.log(`Example app listening on port ${port}!`),
